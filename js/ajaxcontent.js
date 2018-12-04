846b4958ac04fbbed7399328b57e74fc2eb382b8
yingyu <ssaiyuiki15@gmail.com>
2018-12-04 22:31:04 +0800

    ajax測試

---
+++ js/ajaxcontent.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("#asidemenu").html("Loading...");
+    
+    var pagelink = "component";
+    var url = pagelink + "asidemenu.html";
+    
+    $.ajax({
+        async: true,
+        url : url,
+        type : "GET",
+        dataType : "html"
+    }).done(function(data) {        
+        $("#asidemenu").html(data);
+        $("#asidemenu script").each(function() {
+            var jsLink = pagelink + $(this).attr('src');
+            $("head").append("<script src='" + jsLink + "' ></script>")
+        });
+    });
+
+    $('#foot').load('pages/firstpage/footer.html');
+});
