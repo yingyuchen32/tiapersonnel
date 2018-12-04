f4a3364471c0e5f0cd6f36cdc05e03b29547ba6f
yingyu <ssaiyuiki15@gmail.com>
2018-12-04 22:38:21 +0800

    ajax test

---
+++ component/main.js
@@ -0,0 +1,102 @@
+var accordion = (function() {
+
+    var $accordion = $('.js-accordion');
+    var $accordion_header = $accordion.find('.js-accordion-header');
+    var $accordion_item = $('.js-accordion-item');
+
+    // default settings 
+    var settings = {
+        // animation speed
+        speed: 400,
+
+        // close all other accordion items if true
+        oneOpen: false
+    };
+
+    return {
+        // pass configurable object literal
+        init: function($settings) {
+            $accordion_header.on('click', function() {
+                accordion.toggle($(this));
+            });
+
+            $.extend(settings, $settings);
+
+            // ensure only one accordion is active if oneOpen is true
+            if (settings.oneOpen && $('.js-accordion-item.active').length > 1) {
+                $('.js-accordion-item.active:not(:first)').removeClass('active');
+            }
+
+            // reveal the active accordion bodies
+            $('.js-accordion-item.active').find('> .js-accordion-body').show();
+        },
+        toggle: function($this) {
+
+            if (settings.oneOpen && $this[0] != $this.closest('.js-accordion').find('> .js-accordion-item.active > .js-accordion-header')[0]) {
+                $this.closest('.js-accordion')
+                    .find('> .js-accordion-item')
+                    .removeClass('active')
+                    .find('.js-accordion-body')
+                    .slideUp()
+            }
+
+            // show/hide the clicked accordion item
+            $this.closest('.js-accordion-item').toggleClass('active');
+            $this.next().stop().slideToggle(settings.speed);
+        }
+    }
+})();
+
+$(document).ready(function() {
+    accordion.init({ speed: 300, oneOpen: true });
+});
+
+
+var $container = $('.dropdown-menu'),
+    $list = $('.dropdown-menu ul'),
+    listItem = $list.find('li');
+
+$(".dropdown .title").click(function () {
+  if( $container.height() > 0) {
+    closeMenu(this);
+  } else {
+    openMenu(this);
+  }
+});
+
+$(".dropdown-menu li").click(function () {
+  closeMenu(this);
+});
+
+function closeMenu(el) {
+  $(el).closest('.dropdown').toggleClass("closed").find(".title").text($(el).text());
+  $container.css("height", 0);
+  $list.css( "top", 0 );
+}
+
+function openMenu(el) {
+  $(el).parent().toggleClass("closed");
+  
+  $container.css({
+    height: 200
+  })
+  .mousemove(function(e) {
+    var heightDiff = $list.height() / $container.height(),
+        offset = $container.offset(),
+        relativeY = (e.pageY - offset.top),
+        top = relativeY*heightDiff > $list.height()-$container.height() ?
+              $list.height()-$container.height() : relativeY*heightDiff;
+
+    $list.css("top", -top);
+  });
+}
+
+$(function() {
+    var url = window.location.pathname,
+        urlRegExp = new RegExp(url.replace(/\/$/, '') + "$");
+    $('a').each(function() {
+        if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
+            $(this).addClass('active');
+        }
+    });
+});
