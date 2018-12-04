fbca4c86bce6cfcafcae847e77dda8c5948be722
yingyu <ssaiyuiki15@gmail.com>
2018-12-04 23:25:10 +0800

    test

--- js/ajaxcontent.js
+++ js/ajaxcontent.js
@@ -1,21 +1,3 @@
 $(document).ready(function() {
-    $("#asidemenu").html("Loading...");
-    
-    var pagelink = "component/";
-    var url = pagelink + "asidemenu.html";
-    
-    $.ajax({
-        async: true,
-        url : url,
-        type : "GET",
-        dataType : "html"
-    }).done(function(data) {        
-        $("#asidemenu").html(data);
-        $("#asidemenu script").each(function() {
-            var jsLink = pagelink + $(this).attr('src');
-            $("head").append("<script src='" + jsLink + "' ></script>")
-        });
-    });
-
-    $('#foot').load('pages/firstpage/footer.html');
+    $('#asidemenu').load('../component/asidemenu.html');
 });
